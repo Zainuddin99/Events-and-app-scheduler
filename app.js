337672e89bf668642220c617054b4246c37b31c0
Zainuddin99 <zainuzayn11@gmail.com>
2021-08-04 12:00:43 +0530

    fixed postbuild code

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ if(process.env.NODE_ENV === 'production'){
 
     //Provides front end
     app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build', 'index.js'))
+        res.sendFile(path.resolve(__dirname,'client','build', 'index.html'))
     })
 }
 
