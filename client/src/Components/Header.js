b820423b5fcb0140953fe236474e642aa382f367
Zainuddin99 <zainuzayn11@gmail.com>
2023-06-18 21:56:58 +0530

    remove unused package, update deploy command & header update

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -2,37 +2,37 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useGlobalContext } from '../context'
 
 function Header() {
-    const {utilsState, logout} = useGlobalContext()
+    const { utilsState, logout } = useGlobalContext()
     const [downbarOpen, setDownBar] = useState(false)
     const profileElement = useRef(null)
     const downbarElement = useRef(null)
 
-const changeDownbarPosition = () =>{
+    const changeDownbarPosition = () => {
         const position = profileElement.current.getBoundingClientRect()
-        const {top,left, right} = position
-        const center = (left+right)/2
-        downbarElement.current.style.left = `${center-58}px`
-        downbarElement.current.style.top = `${top+30}px`
+        const { top, left, right } = position
+        const center = (left + right) / 2
+        downbarElement.current.style.left = `${center - 58}px`
+        downbarElement.current.style.top = `${top + 30}px`
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         window.addEventListener('resize', changeDownbarPosition)
 
-        return ()=>window.removeEventListener('resize', changeDownbarPosition)
+        return () => window.removeEventListener('resize', changeDownbarPosition)
     }
-)
+    )
 
-useEffect(changeDownbarPosition, [utilsState.fullName])
+    useEffect(changeDownbarPosition, [utilsState.fullName])
 
     return (
         <>
-        <div className='header'>
-            <h1>Daily scheduler</h1>
-            <p className='btn-name' onMouseEnter={()=>setDownBar(true)} ref={profileElement} onMouseLeave={()=>setDownBar(false)}>{utilsState.fullName ? utilsState.fullName : 'Login'}</p>
-        </div>
-            <ul className={`profile-downbar ${downbarOpen ? 'open' : ''}`} ref={downbarElement} onMouseEnter={()=>setDownBar(true)} onMouseLeave={()=>setDownBar(false)}>
-                    <li>View profile</li>
-                    <li onClick={logout}>Logout</li>
+            <div className='header'>
+                <h1>Daily Apps scheduler</h1>
+                <p className='btn-name' onMouseEnter={() => setDownBar(true)} ref={profileElement} onMouseLeave={() => setDownBar(false)}>{utilsState.fullName ? utilsState.fullName : 'Login'}</p>
+            </div>
+            <ul className={`profile-downbar ${downbarOpen ? 'open' : ''}`} ref={downbarElement} onMouseEnter={() => setDownBar(true)} onMouseLeave={() => setDownBar(false)}>
+                <li>View profile</li>
+                <li onClick={logout}>Logout</li>
             </ul>
         </>
     )
--- client/src/context.js
+++ client/src/context.js
@@ -5,133 +5,133 @@ import { utilsStateReducer } from './UtilsStateReducer'
 
 const myContext = createContext()
 
-const initialWeeks = {Mo : false, Tu : false, We : false, Th : false, Fr : false, Sa : false, Su : false }
+const initialWeeks = { Mo: false, Tu: false, We: false, Th: false, Fr: false, Sa: false, Su: false }
 
-const initialUtilsState = {loginPageOpen: false, loginPageState: true, fullName:''}
+const initialUtilsState = { loginPageOpen: false, loginPageState: true, fullName: '' }
 
-const AppProvider = ({children}) =>{
+const AppProvider = ({ children }) => {
     const [weeks, setWeeks] = useState(initialWeeks)
-    const [fetchingState, dispatch] = useReducer(reducer, {loading: false, message: ''})
+    const [fetchingState, dispatch] = useReducer(reducer, { loading: false, message: '' })
     const [allSchedules, setAllScedules] = useState([])
     const [blockedApps, setBlockedApss] = useState([])
     const [limitedApps, setLimitedApps] = useState([])
     const [saveError, setSaveError] = useState('')
     const [utilsState, utilsStateDispatch] = useReducer(utilsStateReducer, initialUtilsState)
-    const [inputs, setInput] = useState({firstName: '', lastName: '', userName: '', password: ''})
+    const [inputs, setInput] = useState({ firstName: '', lastName: '', userName: '', password: '' })
     const [formError, setFormError] = useState(null)
     const [formSuccessMessage, setFormSuccessMessage] = useState('')
 
-    const getSchedulesFromApi = useCallback(async() =>{
-        dispatch({type: 'LOADING'})
+    const getSchedulesFromApi = useCallback(async () => {
+        dispatch({ type: 'LOADING' })
         try {
             const response = await getAllSchedules()
             setAllScedules(response.data.result.schedules)
-            dispatch({type:'SUCCESS', payload: response})
-            utilsStateDispatch({type: 'UPDATE_NAME', payload: response.data.fullName})
+            dispatch({ type: 'SUCCESS', payload: response })
+            utilsStateDispatch({ type: 'UPDATE_NAME', payload: response.data.fullName })
         } catch (error) {
             handleError(error)
         }
     }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         getSchedulesFromApi()
     }, [getSchedulesFromApi])
 
-    useEffect(()=>{
-        const timer = setTimeout(()=>{
-            if(formError) setFormError('')
-            if(formSuccessMessage) setFormSuccessMessage('')
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (formError) setFormError('')
+            if (formSuccessMessage) setFormSuccessMessage('')
         }, 3000)
 
-        return ()=>clearTimeout(timer)
+        return () => clearTimeout(timer)
 
     }, [formError, formSuccessMessage])
 
-    const handleSave = async(start, end) =>{
-        if(start >= end){
+    const handleSave = async (start, end) => {
+        if (start >= end) {
             return setSaveError('Invalid time')
         }
-        if(saveError){
+        if (saveError) {
             setSaveError('')
         }
-        let days = Object.entries(weeks).map(([key, value])=>{
-            if(value){
+        let days = Object.entries(weeks).map(([key, value]) => {
+            if (value) {
                 return key
             }
             return null
         })
-        days = days.filter((item)=>item)
-        dispatch({type: 'LOADING'})
-        try{
-        const data = await sendSchedules({days, startTime: start, endTime: end})
-        dispatch({type:'SUCCESS', payload: data})
-        setWeeks(initialWeeks)
-        window.history.back()
-        getSchedulesFromApi()
-        }catch(err){
-            dispatch({type: 'FAILED', payload: err})
+        days = days.filter((item) => item)
+        dispatch({ type: 'LOADING' })
+        try {
+            const data = await sendSchedules({ days, startTime: start, endTime: end })
+            dispatch({ type: 'SUCCESS', payload: data })
+            setWeeks(initialWeeks)
+            window.history.back()
+            getSchedulesFromApi()
+        } catch (err) {
+            dispatch({ type: 'FAILED', payload: err })
         }
     }
 
-    useEffect(()=>{
-        const timer = setTimeout(()=>dispatch({type: 'CLEAR'}), 3000)
+    useEffect(() => {
+        const timer = setTimeout(() => dispatch({ type: 'CLEAR' }), 3000)
 
-        return ()=>clearTimeout(timer)
+        return () => clearTimeout(timer)
     }, [fetchingState.message])
 
-    const handleError = (err) =>{
-        if(err.response?.status === 401){
-            utilsStateDispatch({type: 'NOT_AUTHORIZED'})
-            return dispatch({type: 'STOP LOADING'})
+    const handleError = (err) => {
+        if (err.response?.status === 401) {
+            utilsStateDispatch({ type: 'NOT_AUTHORIZED' })
+            return dispatch({ type: 'STOP LOADING' })
         }
-        dispatch({type: 'FAILED'})
+        dispatch({ type: 'FAILED' })
     }
 
-    const deleteScheduleFromApi = async(id) =>{
-        try{
+    const deleteScheduleFromApi = async (id) => {
+        try {
             await deleteSchedule(id)
             getSchedulesFromApi()
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
     }
 
-    const getBlockedAppsFromApi = async(id) =>{
-        try{
-        dispatch({type: 'LOADING'})
-        const response = await getBlockedApps(id)
-        setBlockedApss(response.data.result)
-        dispatch({type: 'STOP LOADING'})
-    }catch(err){
-        dispatch({type: 'STOP LOADING'})
-        console.log(err);
-    }
+    const getBlockedAppsFromApi = async (id) => {
+        try {
+            dispatch({ type: 'LOADING' })
+            const response = await getBlockedApps(id)
+            setBlockedApss(response.data.result)
+            dispatch({ type: 'STOP LOADING' })
+        } catch (err) {
+            dispatch({ type: 'STOP LOADING' })
+            console.log(err);
+        }
     }
 
-    const getAllLimitedAppsFromApi =  useCallback(async(id) =>{
-        dispatch({type: 'LOADING'})
+    const getAllLimitedAppsFromApi = useCallback(async (id) => {
+        dispatch({ type: 'LOADING' })
         try {
             const response = await getLimitedApps(id)
             setLimitedApps(response.data.result)
-            dispatch({type: 'STOP LOADING'})
+            dispatch({ type: 'STOP LOADING' })
         } catch (error) {
-            dispatch({type: 'STOP LOADING'})
+            dispatch({ type: 'STOP LOADING' })
             console.log(error)
         }
     }, [])
 
-    const addLimitedAppToApi = async(id, data) =>{
-        try{
+    const addLimitedAppToApi = async (id, data) => {
+        try {
             await addLimitedApp(id, data)
             getAllLimitedAppsFromApi(id)
-        }catch(err){
+        } catch (err) {
             console.log(err);
         }
     }
 
 
-    const handleWeeks = (week) =>{
-        setWeeks((prev)=>{
+    const handleWeeks = (week) => {
+        setWeeks((prev) => {
             return {
                 ...prev,
                 [week]: !(prev?.[week])
@@ -139,29 +139,29 @@ const AppProvider = ({children}) =>{
         })
     }
 
-    const handleChange = (e) =>{
-        setInput((prev)=>{
-            return {...prev, [e.target.name]: e.target.value}
+    const handleChange = (e) => {
+        setInput((prev) => {
+            return { ...prev, [e.target.name]: e.target.value }
         })
     }
 
-    const handleSubmit = async(e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        const {firstName, lastName, password, userName:username} = inputs
-        if(utilsState.loginPageState){
-            if(!username || !password){
+        const { firstName, lastName, password, userName: username } = inputs
+        if (utilsState.loginPageState) {
+            if (!username || !password) {
                 setFormError('All fields are required!')
-            }else{
-                try{
-                const response = await loginUser({username, password})
-                const {token} = response.data
-                localStorage.setItem('token', token)
-                utilsStateDispatch({type: "CLOSE_LOGIN_PAGE"})
-                getSchedulesFromApi()
-                }catch(err){
-                    if(err.response.status === 401){
+            } else {
+                try {
+                    const response = await loginUser({ username, password })
+                    const { token } = response.data
+                    localStorage.setItem('token', token)
+                    utilsStateDispatch({ type: "CLOSE_LOGIN_PAGE" })
+                    getSchedulesFromApi()
+                } catch (err) {
+                    if (err.response.status === 401) {
                         setFormError('Invalid credentials!')
-                    }else{
+                    } else {
                         setFormError('Something went wrong!')
                     }
                 }
@@ -169,35 +169,37 @@ const AppProvider = ({children}) =>{
             return
         }
 
-        if(!firstName || !lastName || !password || !username){
+        if (!firstName || !lastName || !password || !username) {
             return setFormError('All fields are required')
         }
 
-        try{
-            await signUpUser({username, firstName, lastName, password})
+        try {
+            await signUpUser({ username, firstName, lastName, password })
             setFormSuccessMessage('Successfully Signed up')
-        }catch(err){
+        } catch (err) {
             setFormError(err.response.data.message)
         }
 
     }
 
-    const logout = () =>{
-        if(window.confirm('Confirm to logout')){
-        localStorage.clear('token')
-        window.location.reload()
+    const logout = () => {
+        if (window.confirm('Confirm to logout')) {
+            localStorage.clear('token')
+            window.location.reload()
         }
     }
 
-    return (<myContext.Provider value={{weeks, setWeeks, handleWeeks, handleSave, fetchingState, allSchedules, deleteScheduleFromApi, 
-    blockedApps, limitedApps, getSchedulesFromApi, getBlockedAppsFromApi, getAllLimitedAppsFromApi, addLimitedAppToApi, dispatch, saveError,
-    utilsState, utilsStateDispatch, inputs, handleChange, handleSubmit, formError, formSuccessMessage, logout}}>
+    return (<myContext.Provider value={{
+        weeks, setWeeks, handleWeeks, handleSave, fetchingState, allSchedules, deleteScheduleFromApi,
+        blockedApps, limitedApps, getSchedulesFromApi, getBlockedAppsFromApi, getAllLimitedAppsFromApi, addLimitedAppToApi, dispatch, saveError,
+        utilsState, utilsStateDispatch, inputs, handleChange, handleSubmit, formError, formSuccessMessage, logout
+    }}>
         {children}
     </myContext.Provider>)
 }
 
-const useGlobalContext = () =>{
+const useGlobalContext = () => {
     return useContext(myContext)
 }
 
-export {useGlobalContext, AppProvider}
+export { useGlobalContext, AppProvider }
